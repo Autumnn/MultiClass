de5182412071dd8eeeb81056f310567cbdffbbb6
Autumnn <wqiushi@gmail.com>
2018-03-23 17:54:44 +0800

    01

--- 2D_Distribution.py
+++ 2D_Distribution.py
@@ -12,7 +12,7 @@ for i in range(10):
     for j in range(10):
         if i != j:
             fig = plt.figure()
-            p1 = plt.hist2d(RD.Negative_Feature[:,i], RD.Negative_Feature[:,j], bins = 30)
+            p1 = plt.hist2d(RD.Features[:,i], RD.Features[:,j], bins = 30)
 
             File_name = "2D_Distributions_of_" + str(i) + "_and_" + str(j) + "_Feature.png"
             fig.savefig(File_name)
