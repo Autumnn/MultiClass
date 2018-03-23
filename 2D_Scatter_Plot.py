94d7035ba6325b1d2f4703b7a202b15c0acc0c14
Autumnn <wqiushi@gmail.com>
2018-03-23 17:24:18 +0800

    01

--- 2D_Scatter_Plot.py
+++ 2D_Scatter_Plot.py
@@ -8,12 +8,15 @@ dir = "page-blocks0.dat"
 
 RD.Initialize_Data(dir)
 
-fig = plt.figure()
 
-p1 = plt.scatter(RD.Negative_Feature[:,4], RD.Negative_Feature[:,5], marker = 'o', color = '#539caf', label='1', s = 10, alpha=0.3)
 
-p2 = plt.scatter(RD.Positive_Feature[:,4], RD.Positive_Feature[:,5], marker = '+', color = 'r', label='2', s = 50)
+for i in range(10):
+    for j in range(10):
+        if i != j:
+            fig = plt.figure()
+            p1 = plt.scatter(RD.Negative_Feature[:,i], RD.Negative_Feature[:,j], marker = 'o', color = '#539caf', label='1', s = 10, alpha=0.3)
+            p2 = plt.scatter(RD.Positive_Feature[:,i], RD.Positive_Feature[:,j], marker = '+', color = 'r', label='2', s = 50)
+            File_name = "Scatter_Plot_of_" + str(i) + "_and_" + str(j) + "_Feature.png"
+            fig.savefig(File_name)
 
-File_name = "Scatter_Plot_of_" + str(4) + "_and_" + str(5) + "_Feature.png"
 
-fig.savefig(File_name)
--- 3D_Scatter_Plot.py
+++ 3D_Scatter_Plot.py
@@ -9,7 +9,7 @@ dir = "page-blocks0.dat"
 RD.Initialize_Data(dir)
 
 ax = plt.subplot(111, projection='3d')
-ax.scatter(RD.Negative_Feature[:,4], RD.Negative_Feature[:,5], RD.Negative_Feature[:,1], marker = 'o', color = '#539caf', label='1', s = 10, alpha=0.3)
-ax.scatter(RD.Positive_Feature[:,4], RD.Positive_Feature[:,5], RD.Positive_Feature[:,1], marker = '+', color = 'r', label='2', s = 50)
+ax.scatter(RD.Negative_Feature[:,4], RD.Negative_Feature[:,5], RD.Negative_Feature[:,3], marker = 'o', color = '#539caf', label='1', s = 10, alpha=0.3)
+ax.scatter(RD.Positive_Feature[:,4], RD.Positive_Feature[:,5], RD.Positive_Feature[:,3], marker = '+', color = 'r', label='2', s = 50)
 
 plt.show()
