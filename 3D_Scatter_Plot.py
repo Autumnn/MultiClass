7044ba71576bde5e646bfad62f2cc627dc8cd771
Autumnn <wqiushi@gmail.com>
2018-03-29 14:19:04 +0800

    01

--- 3D_Scatter_Plot.py
+++ 3D_Scatter_Plot.py
@@ -4,13 +4,13 @@ from mpl_toolkits.mplot3d import Axes3D
 import Read_Data as RD
 
 #dir = "wine-5-fold/wine-5-1tra.dat"
-dir = "segment0.dat"
+dir = "page-blocks0.dat"
 
 RD.Initialize_Data(dir)
 
-for i in range(14, 19):
-    for j in range(i+1, 19):
-        for k in range(j+1, 19):
+for i in range(0, RD.Num_Features):
+    for j in range(i+1, RD.Num_Features):
+        for k in range(j+1, RD.Num_Features):
             print(i, j ,k)
             ax = plt.subplot(111, projection='3d')
             ax.scatter(RD.Negative_Feature[:,i], RD.Negative_Feature[:,j], RD.Negative_Feature[:,k], marker = 'o', color = '#539caf', label='1', s = 30, alpha=0.3)
